5a6dbf925572e3fe6c776c97a32cc7501c4c6be0
Хатламаджиян Виталий <vitaly@orbita1.ru>
2015-08-17 10:46:04 +0300

    Состояние на 17.08.2015

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,7 +3,9 @@
 const
     Cc = Components.classes,
     Ci = Components.interfaces,
-    Cu = Components.utils;
+    Cu = Components.utils,
+    XMLHttpRequest  = Components.Constructor("@mozilla.org/xmlextras/xmlhttprequest;1", "nsIXMLHttpRequest"),
+    Notification = Components.Constructor("@mozilla.org/xmlextras/xmlhttprequest;1", "nsIDOMDesktopNotification");
 
 Cu.import("resource://gre/modules/Services.jsm");
 
@@ -104,6 +106,12 @@ var ASSO = {
 ASSO.init = function () {
     ASSO.timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
     ASSO.timer.initWithCallback(function (timer) {ASSO.getContent();}, 15000, Ci.nsITimer.TYPE_REPEATING_PRECISE);
+
+    var apiKey = this.getPref("api_key", "char");
+    if (apiKey == "") {
+        //Services.wm.getMostRecentWindow('navigator:browser').BrowserOpenAddonsMgr();
+        Services.wm.getMostRecentWindow('navigator:browser').BrowserOpenAddonsMgr('addons://detail/asso_notifier/preferences');
+    }
 };
 
 ASSO.getProp = function (id) {
@@ -158,42 +166,29 @@ ASSO.setPref = function (id, value, type) {
                 return this.prefManager.setBoolPref(id, value);
         }
     } catch (e) {
-        console.error(e.message);
+        // console.error(e.message);
     }
 };
 
-ASSO.getContent = function () {
-    var apiKey = this.getPref("api_key", "char");
-    if (apiKey == "") {
-        // TODO: Открыть диалог ввода ключа, если значение не установлено
-        return ;
-    }
-    var today = Date.now();
-    if (today < this.getPref("last_order_date", "int")) {
-        return ;
-    }
-    var assoApiBaseUrl = "http://asso.orbitsoft/api",
-        urlStatus = assoApiBaseUrl + "/dinners/menu/status?access_token=" + encodeURIComponent(apiKey),
-        urlMy = assoApiBaseUrl + "/orders/today/my?access_token=" + encodeURIComponent(apiKey);
+ASSO.ajax = function (url, success, failure) {
     try {
         var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function () {
+        if (success || failure) {
+            xmlhttp.onreadystatechange = function () {
                 if (xmlhttp.readyState == 4) {
                     if (xmlhttp.status == 200) {
-                        //SQUID.parseStat(xmlhttp.responseText);
-                        var data = JSON.parse(xmlhttp.response);
-                        /* TODO: протестировать ответ
-                        if (data.status == "ok") {
-
+                        if (success) {
+                            success(xmlhttp.response, xmlhttp);
                         }
-                        */
-                        ASSO.setPref("last_order_date", today, "int");
-                    } else if (xmlhttp.status >= 400) {
-                        console.error(xmlhttp.responseText);
+                    } else if (xmlhttp.status >= 400 || xmlhttp.status < 100) {
+                        if (failure) {
+                            failure(xmlhttp.response, xmlhttp);
+                        }
                     }
                 }
             };
-        xmlhttp.open("GET", urlStatus, true);
+        }
+        xmlhttp.open("GET", url, true);
         xmlhttp.setRequestHeader("Content-Type", "text/html; charset=UTF-8");
         xmlhttp.setRequestHeader("Expires", "Sat, 26 Jul 1997 05:00:00 GMT");
         xmlhttp.setRequestHeader("Last-Modified", "Sat, 26 Jul 1997 05:00:00 GMT");
@@ -203,7 +198,54 @@ ASSO.getContent = function () {
         xmlhttp.setRequestHeader("Expires", "0");
         xmlhttp.send(null);
     } catch (e) {
+        // console.log(e.message);
         return false;
     }
+};
+
+ASSO.getContent = function () {
+    var apiKey = this.getPref("api_key", "char");
+    if (apiKey == "") {
+        // TODO: Открыть диалог ввода ключа, если значение не установлено
+        return false;
+    }
+    var today = Date.now();
+    if (today < this.getPref("last_order_date", "int")) {
+        return false;
+    }
+    var assoApiBaseUrl = "http://asso.orbitsoft/api",
+        urlStatus = assoApiBaseUrl + "/dinners/menu/status?access_token=" + encodeURIComponent(apiKey),
+        urlMy = assoApiBaseUrl + "/orders/today/my?access_token=" + encodeURIComponent(apiKey);
+    ASSO.ajax(urlStatus, function (data) { // success
+        data = JSON.parse(data);
+        if (data.status == "ok") {
+            if (data.data.is_ready == true) {
+                ASSO.ajax(urlMy, function (data) { // success
+                    data = '{"status":"error"}';
+                    data = JSON.parse(data);
+                    if (data.status == "error") {
+                        console.log("here");
+                        var notification = new Notification("ASSO", {
+                            "tag" : "asso",
+                            "icon" : "chrome://asso_notifier/skin/icon-48.png",
+                            "body" : "Уважаемый, заказываем еду"
+                        });
+                        setTimeout(notification.close.bind(notification), 15000);
+                        notification.onclick = function (event) {
+                            window.open("http://asso.orbitsoft/", "_blank");
+                        }
+                    } else {
+                        ASSO.timer.cancel();
+                    }
+                    ASSO.setPref("last_order_date", today, "int");
+                }, function (data, xmlhttp) { // failure
+                    return false;
+                });
+            }
+        }
+    }, function (data, xmlhttp) { // failure
+        // console.log(data, xmlhttp);
+        return false;
+    });
     return true;
 };
